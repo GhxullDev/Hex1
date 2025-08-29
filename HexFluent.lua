bd33a2bebe63c678ed1858b0d61f196df894fca7
GhxullDev <davide.campanale@icloud.com>
2025-08-29 12:08:20 +0200

    Update HexFluent.lua

--- HexFluent.lua
+++ HexFluent.lua
@@ -1,260 +1,203 @@
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/Beta.lua", true))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
-local Window = Fluent:CreateWindow({
-    Title = "♻ Hex Hub | Cool Rarities 🍀 " .. Fluent.Version,
-    SubTitle = "By Ghxull",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
-})
-
---Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
-
-local Options = Fluent.Options
-
-do
-    Fluent:Notify({
-        Title = "Notification",
-        Content = "This is a notification",
-        SubContent = "SubContent", -- Optional
-        Duration = 5 -- Set to nil to make the notification not disappear
-    })
-
-
-
-    Tabs.Main:AddParagraph({
-        Title = "Paragraph",
-        Content = "This is a paragraph.\nSecond line!"
-    })
-
-
-
-    Tabs.Main:AddButton({
-        Title = "Button",
-        Description = "Very important button",
-        Callback = function()
-            Window:Dialog({
-                Title = "Title",
-                Content = "This is a dialog",
-                Buttons = {
-                    {
-                        Title = "Confirm",
-                        Callback = function()
-                            print("Confirmed the dialog.")
-                        end
-                    },
-                    {
-                        Title = "Cancel",
-                        Callback = function()
-                            print("Cancelled the dialog.")
-                        end
-                    }
-                }
-            })
-        end
-    })
-
-
-
-    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
-
-    Toggle:OnChanged(function()
-        print("Toggle changed:", Options.MyToggle.Value)
-    end)
-
-    Options.MyToggle:SetValue(false)
-
-
     
-    local Slider = Tabs.Main:AddSlider("Slider", {
-        Title = "Slider",
-        Description = "This is a slider",
-        Default = 2,
-        Min = 0,
-        Max = 5,
-        Rounding = 1,
-        Callback = function(Value)
-            print("Slider was changed:", Value)
-        end
+    local Window = Fluent:CreateWindow({
+        Title = "♻ Hex Hub | Cool Rarities 🍀 " .. Fluent.Version,
+        SubTitle = "By Ghxull",
+        Icon = "rbxassetid://124019644799794", -- The icon of the window, can be a URL or an asset ID
+        TabWidth = 160,
+        Size = UDim2.fromOffset(580, 460),
+        Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+        Theme = "Dark",
+        MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
     })
-
-    Slider:OnChanged(function(Value)
-        print("Slider changed:", Value)
-    end)
-
-    Slider:SetValue(3)
-
-
-
-    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
-        Title = "Dropdown",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = false,
-        Default = 1,
-    })
-
-    Dropdown:SetValue("four")
-
-    Dropdown:OnChanged(function(Value)
-        print("Dropdown changed:", Value)
-    end)
-
-
     
-    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
-        Title = "Dropdown",
-        Description = "You can select multiple values.",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = true,
-        Default = {"seven", "twelve"},
-    })
-
-    MultiDropdown:SetValue({
-        three = true,
-        five = true,
-        seven = false
-    })
-
-    MultiDropdown:OnChanged(function(Value)
-        local Values = {}
-        for Value, State in next, Value do
-            table.insert(Values, Value)
-        end
-        print("Mutlidropdown changed:", table.concat(Values, ", "))
-    end)
-
-
-
-    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
-        Title = "Colorpicker",
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    Colorpicker:OnChanged(function()
-        print("Colorpicker changed:", Colorpicker.Value)
-    end)
+    local Tabs = {
+        Infos = Window:AddTab({ Title = "Infos", Icon = "book" }),
+        LocalPlayer = Window:AddTab({ Title = "LocalPlayer", Icon = "user" }),
+        Autofarm = Window:AddTab({ Title = "Autofarm", Icon = "clover" }),
+        Teleport = Window:AddTab({ Title = "Teleport", Icon = "plane"}),
+        Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+    }
     
-    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
+    local Options = Fluent.Options
+    
+    do
+        Fluent:Notify({
+            Title = "Remember",
+            Content = "Don't close the interface.",
+            Duration = 3 -- Set to nil to make the notification not disappear
+        })
+    
+        Tabs.Infos:AddParagraph({
+            Title = "Infos about the script",
+            Content = "The script was made by Ghxull. \nIt's a only Cool Rarities Script, have a nice play using the script."
+        })
+    
+        Tabs.LocalPlayer:AddParagraph({
+            Title = "LocalPlayer Tab",
+            Content = "This is the Local Player tab of the script. \nHere you will find cool features about the game, in a specific way, all about Local Player."
+        })
+    
+        Tabs.Autofarm:AddParagraph({
+            Title = "Autofarm Tab",
+            Content = "This is the Autofarm tab of the script. \nHere you will find cool features about the game, in a more spcific way, all about autofarms."
+        })
+    
+        Tabs.Teleport:AddParagraph({
+            Title = "Teleport Tab",
+            Content = "This is the Teleport tab of the script. \nHere you will find cool features about the game, in a more specific way, all about teleport."
+        })
+    
+        local Section = Tabs.LocalPlayer:AddSection("LocalPlayer","settings")
 
-
-
-    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
-        Title = "Colorpicker",
-        Description = "but you can change the transparency.",
-        Transparency = 0,
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    TColorpicker:OnChanged(function()
-        print(
-            "TColorpicker changed:", TColorpicker.Value,
-            "Transparency:", TColorpicker.Transparency
-        )
-    end)
-
-
-
-    local Keybind = Tabs.Main:AddKeybind("Keybind", {
-        Title = "KeyBind",
-        Mode = "Toggle", -- Always, Toggle, Hold
-        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
-
-        -- Occurs when the keybind is clicked, Value is `true`/`false`
-        Callback = function(Value)
-            print("Keybind clicked!", Value)
-        end,
-
-        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
-        ChangedCallback = function(New)
-            print("Keybind changed!", New)
-        end
-    })
-
-    -- OnClick is only fired when you press the keybind and the mode is Toggle
-    -- Otherwise, you will have to use Keybind:GetState()
-    Keybind:OnClick(function()
-        print("Keybind clicked:", Keybind:GetState())
-    end)
-
-    Keybind:OnChanged(function()
-        print("Keybind changed:", Keybind.Value)
-    end)
-
-    task.spawn(function()
-        while true do
-            wait(1)
-
-            -- example for checking if a keybind is being pressed
-            local state = Keybind:GetState()
-            if state then
-                print("Keybind is being held down")
+        local Slider = Tabs.LocalPlayer:AddSlider("Walkspeed", {
+            Title = "Walkspeed",
+            Description = "Change your Walkspeed",
+            Default = 16,
+            Min = 1,
+            Max = 100,
+            Rounding = 1,
+            Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
             end
+        })
 
-            if Fluent.Unloaded then break end
-        end
-    end)
+        local Slider = Tabs.LocalPlayer:AddSlider("JumpPower", {
+            Title = "JumpPower",
+            Description = "Change your JumpPower",
+            Default = 50,
+            Min = 0,
+            Max = 250,
+            Rounding = 1,
+            Callback = function(Value)
+            game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+            end
+        })
 
-    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
+        local Toggle = Tabs.LocalPlayer:AddToggle("NoClip", {Title = "NoClip", Default = false })
+    
+            Toggle:OnChanged(function()
+                game.Players.LocalPlayer.Character.Humanoid.CanCollide = false
+        end)
+    
+        Options.MyToggle:SetValue(false)
+    
+
+        local Toggle = Tabs.LocalPlayer:AddToggle("InfJump", {Title = "InfJump", Default = false })
+    
+            Toggle:OnChanged(function(Value)
+                if Value then
+                    game:GetService("UserInputService").JumpRequest:connect(function()
+                        game.Players.LocalPlayer.Character.Humanoid:ChangeState("Jumping")
+                    end)
+                end
+        end)
 
 
-    local Input = Tabs.Main:AddInput("Input", {
-        Title = "Input",
-        Default = "Default",
-        Placeholder = "Placeholder",
-        Numeric = false, -- Only allows numbers
-        Finished = false, -- Only calls callback when you press enter
-        Callback = function(Value)
-            print("Input changed:", Value)
-        end
+        Options.MyToggle:SetValue(false)
+
+
+        local Toggle = Tabs.LocalPlayer:AddToggle("Fly", {Title = "Fly", Default = false })
+    
+            Toggle:OnChanged(function(Value)
+                if Value then
+                    local T = game.Players.LocalPlayer.Character.HumanoidRootPart
+                    local CONTROL = {F = 0, B = 0, L = 0, R = 0}
+                    local SPEED = 50
+                    
+                    function FLY()
+                        local BG = Instance.new('BodyGyro', T)
+                        local BV = Instance.new('BodyVelocity', T)
+                        BG.P = 9e4
+                        BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+                        BG.cframe = T.CFrame
+                        BV.velocity = Vector3.new(0, 0, 0)
+                        BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+                        repeat wait()
+                            T.CFrame = T.CFrame * CFrame.new(0, 0, (CONTROL.F + CONTROL.B) * SPEED / 10) * CFrame.Angles(0, math.rad((CONTROL.L + CONTROL.R) * SPEED), 0)
+                            if CONTROL.F + CONTROL.B ~= 0 or CONTROL.L + CONTROL.R ~= 0 then
+                                BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B) * .2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+                            else
+                                BV.velocity = Vector3.new(0, 0, 0)
+                            end
+                            BG.cframe = workspace.CurrentCamera.CoordinateFrame
+                        until not Value
+                        CONTROL = {F = 0, B = 0, L = 0, R = 0}
+                        SPEED = 50
+                        BG:Destroy()
+                        BV:Destroy()
+                    end
+                    
+                    FLY()
+                    
+                    game:GetService('UserInputService').InputBegan:Connect(function(KEY)
+                        if KEY.KeyCode == Enum.KeyCode.W then
+                            CONTROL.F = 1
+                        elseif KEY.KeyCode == Enum.KeyCode.S then
+                            CONTROL.B = -1
+                        elseif KEY.KeyCode == Enum.KeyCode.A then
+                            CONTROL.L = -1
+                        elseif KEY.KeyCode == Enum.KeyCode.D then
+                            CONTROL.R = 1 
+                        end
+                    end)
+                end
+            end)
+
+        Options.MyToggle:SetValue(false)
+
+
+
+        local Section = Tabs.Autofarm:AddSection("Autofarm","code-xml")
+
+        Tabs.Autofarm:AddButton({
+            Title = "CollectChest",
+            Description = "It collect every chest of the game",
+            Callback = function()
+                local player = game.Players.LocalPlayer
+                local chestFolder = workspace:FindFirstChild("Chests")
+                if chestFolder then
+                    for _, chest in ipairs(chestFolder:GetChildren()) do
+                        if chest:IsA("BasePart") then
+                            firetouchinterest(player.Character.HumanoidRootPart, chest, 0)
+                            firetouchinterest(player.Character.HumanoidRootPart, chest, 1)
+                        end
+                    end
+                end
+            end
+        })
+
+    local Section = Tabs.Teleport:AddSection("Teleport","plane")
+        
+    
+        local Dropdown = Tabs.Teleport:AddDropdown("Dropdown", {
+            Title = "Teleport to Zone",
+            Values = {"CloverRune", "RollRarity", "CloverUpg", "BaseLuckZone2", "Prestige Rune", "BaseLuck Rune"},
+            Multi = false,
+            Default = nil,
+        })
+    
+        Dropdown:OnChanged(function(Value)
+            print("Dropdown changed:", Value)
+        end)
+    
+    
+    SaveManager:SetLibrary(Fluent)
+    InterfaceManager:SetLibrary(Fluent)
+    SaveManager:IgnoreThemeSettings()
+    SaveManager:SetIgnoreIndexes({})
+    InterfaceManager:SetFolder("FluentScriptHub")
+    SaveManager:SetFolder("FluentScriptHub/specific-game")
+    InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+    SaveManager:BuildConfigSection(Tabs.Settings)
+    Window:SelectTab(1)
+    Fluent:Notify({
+        Title = "Hex Hub",
+        Content = "The script has been loaded correctly.",
+        Duration = 5
     })
+    SaveManager:LoadAutoloadConfig()
 
-    Input:OnChanged(function()
-        print("Input updated:", Input.Value)
-    end)
 end
-
-
--- Addons:
--- SaveManager (Allows you to have a configuration system)
--- InterfaceManager (Allows you to have a interface managment system)
-
--- Hand the library over to our managers
-SaveManager:SetLibrary(Fluent)
-InterfaceManager:SetLibrary(Fluent)
-
--- Ignore keys that are used by ThemeManager.
--- (we dont want configs to save themes, do we?)
-SaveManager:IgnoreThemeSettings()
-
--- You can add indexes of elements the save manager should ignore
-SaveManager:SetIgnoreIndexes({})
-
--- use case for doing it this way:
--- a script hub could have themes in a global folder
--- and game configs in a separate folder per game
-InterfaceManager:SetFolder("FluentScriptHub")
-SaveManager:SetFolder("FluentScriptHub/specific-game")
-
-InterfaceManager:BuildInterfaceSection(Tabs.Settings)
-SaveManager:BuildConfigSection(Tabs.Settings)
-
-
-Window:SelectTab(1)
-
-Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
-})
-
--- You can use the SaveManager:LoadAutoloadConfig() to load a config
--- which has been marked to be one that auto loads!
-SaveManager:LoadAutoloadConfig()
